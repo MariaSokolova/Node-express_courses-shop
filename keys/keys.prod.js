a23cbcfc6921ed0c925e5d02bd9359878fe683bd
Maria Sokolova <sb_mv@inbox.ru>
2020-11-09 12:21:53 +0100

    feat: clean the code

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,6 +1,5 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
-  // MONGODB_URI: `mongodb+srv://Mariia:P5L0Vuv8BkslxCaF@cluster0.eyx9s.mongodb.net/shop`,
   SESSION_SECRET: process.env.SESSION_SECRET,
   BASE_URL: process.env.BASE_URL
 };
